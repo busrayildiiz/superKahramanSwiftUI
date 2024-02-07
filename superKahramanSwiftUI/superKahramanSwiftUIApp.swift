6118025e3f79cc12eadd17e773fc00448434d93d
missminyoon3 <47670662+missminyoon3@users.noreply.github.com>
2024-02-07 15:26:31 +0300

    Initial Commit

---
+++ superKahramanSwiftUI/superKahramanSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  superKahramanSwiftUIApp.swift
+//  superKahramanSwiftUI
+//
+//  Created by MacBook Pro on 7.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct superKahramanSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
